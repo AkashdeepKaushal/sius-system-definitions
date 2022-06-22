3657d99f638a16c9f0dc65d7d39c1d91a84ff716
AkashdeepKaushal <akashdeep.kaushal@gmail.com>
2022-06-22 11:41:23 -0400

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,61 @@
-import { ElementBaseDefinition, Option, UIValidation } from 'sius-core-types';
+import { ElementBaseDefinition, FieldSetBaseDefinition, Option, UIValidation } from 'sius-core-types';
 export const SystemType_Base: ElementBaseDefinition = {Name: 'Type', Label: 'System Type', Type: 'input', Type2: 'text', ControlType: 'text', 
 Size: 55, Default: '',
     Validations: [ 
         new UIValidation('Required', 'Required', 'is required'),
      ],
-    AsyncValidations: [],
+    AsyncValidations: [
+    ],
     Options: []
 };
 export const SystemTypeName_Base: ElementBaseDefinition = {Name: 'Value', Label: 'Name', Type: 'input', Type2: 'text', ControlType: 'text', 
 Size: 55, Default: '',
-    Validations: [],
+    Validations: [
+        new UIValidation('Required', 'Required', 'is required')
+    ],
     AsyncValidations: [],
     Options: []
 };
 
 export const SystemSubtype_Base: ElementBaseDefinition = {Name: 'Subtype', Label: 'System Sub-Type', Type: 'input', Type2: 'text', ControlType: 'text', 
 Size: 55, Default: '',
-    Validations: [],
+    Validations: [
+        new UIValidation('Required', 'Required', 'is required')
+    ],
     AsyncValidations: [],
     Options: []
 };
 
 export const Description_Base: ElementBaseDefinition = {Name: 'Description', Label: 'Addl. Info', Type: 'input', Type2: 'text', ControlType: 'text', 
 Size: 55, Default: '',
-    Validations: [],
+    Validations: [
+    ],
     AsyncValidations: [],
     Options: []
 };
+
+export const SystemDefinitionFS: FieldSetBaseDefinition<ElementBaseDefinition> = 
+new FieldSetBaseDefinition('SystemDefinition', 'FORMGROUP', 'SystemDefinition', 
+[
+    SystemType_Base,
+    SystemTypeName_Base,
+    SystemSubtype_Base,
+    Description_Base
+
+], '100%', '100%', [], [], [
+    new UIValidation('Exists', 'Exists', 'already exists', ['Type', 'Value', 'SubType'])
+]);
+
+export interface SystemDefinition {
+    Type: string;
+    Value: string;
+    SubType: string;
+    Description: string;
+}
+
+export interface SystemDefinitionExists {
+    Type: string;
+    Value: string;
+    SubType: string;
+}
+
